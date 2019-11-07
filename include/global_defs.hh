860c1387ab676823dfc9c0dd172e24b3c4a89692
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-11-07 19:45:51 +0100

    Add missing #endif, remove wrong const in global_defs

--- include/global_defs.hh
+++ include/global_defs.hh
@@ -15,15 +15,15 @@ constexpr int SUN_N = __SUN_N__;
 constexpr int SUN_elems = 2 * SUN_N * SUN_N;
 constexpr int SUN_dim = SUN_N * SUN_N - 1;
 
-inline void Gauge_Field_Alloc(double*& gauge_field, int T, int L) const {
+inline void Gauge_Field_Alloc(double*& gauge_field, int T, int L) {
 	gauge_field = new double[T * L * L * L * 4 * SUN_elems];
 }
 
-inline void Gauge_Field_Free(double*& gauge_field) const {
+inline void Gauge_Field_Free(double*& gauge_field) {
 	delete[] gauge_field;
 }
 
-inline void Gauge_Field_Copy(const double* dest, const double* src, int T, int L) const {
+inline void Gauge_Field_Copy(double* dest, const double* src, int T, int L) {
 	std::copy(src, src + T * L * L * L * 4 * SUN_elems, dest);
 }
 
