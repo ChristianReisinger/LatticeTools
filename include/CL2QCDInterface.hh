1cc205b2d8fc9920bd7a48c73935eaab4fb00409
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-11-13 17:54:35 +0100

    Implement pImpl pattern for CL2QCDInterface

--- include/CL2QCDInterface.hh
+++ include/CL2QCDInterface.hh
@@ -1,14 +1,6 @@
 #include <set>
 #include <memory>
 
-#include <meta/inputparameters.hpp>
-#include <hardware/hardwareParameters.hpp>
-#include <hardware/openClKernelParameters.hpp>
-#include <hardware/system.hpp>
-#include <physics/interfacesHandler.hpp>
-#include <physics/prngInterface.hpp>
-#include <physics/lattices/gaugefield.hpp>
-
 #include <SUNInterface.hh>
 
 #ifndef INCLUDE_DE_UNI_FRANKFURT_ITP_REISINGER_LATTICETOOLS_0719_CL2QCDINTERFACE_HH_
@@ -39,17 +31,8 @@ public:
 	double get_beta() const override;
 
 private:
-	const int m_overrelax_steps;
-
-	std::unique_ptr<meta::Inputparameters> m_params;
-	std::unique_ptr<const hardware::HardwareParametersInterface> m_hardware_params;
-	std::unique_ptr<const hardware::code::OpenClKernelParametersInterface> m_kernel_params;
-	std::unique_ptr<physics::PrngParametersInterface> m_prng_params;
-	std::unique_ptr<hardware::System> m_system;
-	std::unique_ptr<physics::PRNG> m_prng;
-
-	std::unique_ptr<physics::InterfacesHandler> m_interfaces_handler;
-	std::unique_ptr<physics::lattices::Gaugefield> m_gaugefield;
+	class Implementation;
+	std:: unique_ptr<Implementation> impl;
 };
 
 }
