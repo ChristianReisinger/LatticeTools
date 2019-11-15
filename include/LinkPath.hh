b96400d9406c532aec2a3090666144d9d1eb80c7
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-11-15 17:00:30 +0100

    Define constant N_dim = 4

--- include/LinkPath.hh
+++ include/LinkPath.hh
@@ -13,16 +13,16 @@ class LinkPath {
 public:
 	double path[SUN_elems];
 
-	LinkPath(const double* gauge_field, int T, int L, const std::array<int, 4>& n);
+	LinkPath(const double* gauge_field, int T, int L, const std::array<int, N_dim>& n);
 
 	LinkPath& operator()(int mu, bool pos);
 	LinkPath& move(int mu, int dist);
-	LinkPath& reset(const std::array<int, 4>& new_n);
+	LinkPath& reset(const std::array<int, N_dim>& new_n);
 
 private:
 	const double* gauge_field;
 	int T, L;
-	std::array<int, 4> n;
+	std::array<int, N_dim> n;
 };
 
 }
